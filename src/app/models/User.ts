9c3856517106f0099e60656ee6a4f6c4b2742f01
embarekmessai <m_mebarek@hotmail.com>
2023-02-02 12:25:31 +0100

    Add: Create user model with profile and types

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,94 +1,92 @@
-import { Schema, model } from 'mongoose';
+import { Schema, model, Document } from 'mongoose';
 
-const userSchema = new Schema({
-    firstname: {
-        type: String,
-        required: true
-    },
-    lastname: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
+export type UserDocument = Document & {
+    firstname: string;
+    lastname: string;
+    email: string;
+    password: string;
+    passwordResetToken: string;
+    passwordResetExpires: Date;
+
+    accessToken: string;
+
     profile: {
-        isActive: {
-            type: Boolean,
-            default: false
-        },
-        mobile: {
-            type: String,
-            required: true
-        },
-        mobile_confirmation: {
-            type: Boolean,
-        },
-        mobile_confirmed_at: {
-            type: Date
-        },
-        description: {
-            type: String
-        },
-        address: {
-            type: String
-        },
-        city: {
-            type: String
-        },
-        postal_code: {
-            type: String
-        },
-        country: {
-            type: String
-        },
-        latitude: {
-            type: Number
-        },
-        longitude: {
-            type: Number
-        },
-        avatar: {
-            type: String
-        },
-        last_login: {
-            type: Date
-        },
-        last_logout: {
-            type: Date
-        },
-        is_online: {
-            type: Boolean
-        },
-        photos: [String],
-        presentation: {
-            type: String
-        },
-        whatsapp: {
-            type: String
-        },
-        facebook: {
-            type: String
-        },
-        youtube: {
-            type: String
-        },
-        instagram: {
-            type: String
-        },
-        snapshat: {
-            type: String
-        },
-        tiktok: {
-            type: String
-        }
-    }
-}, { timestamps: true });
+        isActive: Boolean,
+        mobile: string,
+        mobile_confirmation: string,
+        mobile_confirmed_at: string,
+        description: string,
+        address: string,
+        city: string,
+        postal_code: string,
+        country: string,
+        latitude: string,
+        longitude: string,
+        avatar: string,
+        last_login: string,
+        last_logout: string,
+        isOnline: string,
+        photos: string,
+        presentation: string,
+        whatsapp: string,
+        facebook: string,
+        youtube: string,
+        instagram: string,
+        snapshat: string,
+        tiktok: string
+    };
+    createdAt: Date;
+    updatedAt: Date;
+    comparePassword: comparePasswordFunction;
+};
 
-export default model('User', userSchema);
+type comparePasswordFunction = (candidatePassword: string) => boolean;
+
+const userSchema = new Schema<UserDocument>(
+    {
+        firstname: {type: String, required: true},
+        lastname: {type: String, required: true},
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        passwordResetToken: {type: String},
+        passwordResetExpires: {type: Date},
+        accessToken: {type: String},
+
+        profile: {
+            isActive: {type: Boolean, default: false},
+            mobile: {type: String},
+            mobile_confirmation: {type: Boolean},
+            mobile_confirmed_at: {type: Date},
+            description: {type: String},
+            address: {type: String},
+            city: {type: String},
+            postal_code: {type: String},
+            country: {type: String},
+            latitude: {type: Number},
+            longitude: {type: Number},
+            avatar: {type: String},
+            last_login: {type: Date},
+            last_logout: {type: Date},
+            isOnline: {type: Boolean},
+            photos: [String],
+            presentation: {type: String},
+            whatsapp: {type: String},
+            facebook: {type: String},
+            youtube: {type: String},
+            instagram: {type: String},
+            snapshat: {type: String},
+            tiktok:{type: String}
+        }
+    }, { timestamps: true }
+    );
+
+    const comparePassword: comparePasswordFunction = function (candidatePassword) {
+        // Get Hashed Passowrd
+        const hashedPassword = CryptoJS.AES.encrypt(candidatePassword, process.env.PASS_SEC).toString();
+        if( hashedPassword === this.password ) {
+            return true;
+        }
+        return false;
+    };
+
+export const User = model<UserDocument>('User', userSchema);
