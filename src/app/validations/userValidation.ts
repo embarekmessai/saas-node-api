e5bb1cf27c55015286e7ffa20d46c67616b46e3f
embarekmessai <m_mebarek@hotmail.com>
2023-02-02 21:07:15 +0100

    Add: Ceate auth validator with login & register methods

---
+++ src/app/validations/userValidation.ts
@@ -0,0 +1,34 @@
+import { check } from 'express-validator';
+import validate from './Validation';
+
+export const loginValidator = validate([
+    check('email')
+        .isEmail()
+        .withMessage('Invalid email address'),
+    check('password')
+        .isLength({ min: 1 })
+        .withMessage('Password must not be empty')
+]);
+
+export const registerValidator = validate([
+    check('firstname')
+        .isLength({ min: 1 })
+        .withMessage('Firstname must not be empty'),
+    check('lastname')
+        .isLength({ min: 1 })
+        .withMessage('Lastname must not be empty'),
+    check('email')
+        .isEmail()
+        .withMessage(value => `${value} is an invalid email address`)
+        .bail(),
+    check('password')
+        .exists()
+        .isLength({ min: 8 })
+        .withMessage('must be at least 8 chars long')
+        .bail(),
+    check('password_confirmation')
+        .exists()
+        .custom((value, { req }) => value === req.body.password)
+        .withMessage('Password confirmation do not match')
+        .bail(),
+]);
