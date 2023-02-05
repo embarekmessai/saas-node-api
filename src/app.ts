9ba4bd77be9763aac958bcc31a5f60243f8eee85
embarekmessai <m_mebarek@hotmail.com>
2023-02-05 17:44:04 +0100

    Add: Integrate express session package and save it on db

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,60 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import morgan from 'morgan';
 import cors from 'cors';
 import authRouter from './routes/auth';
+import userRouter from './routes/users';
+import session from "express-session";
+import config from './configs/config';
+import MongoStore from "connect-mongo";
+import mongoose from 'mongoose';
+import bluebird from 'bluebird';
+import helmet from 'helmet';
+import CSRF from './configs/tokenize';
 
 var app = express();
 
-// app.use(logger('dev'));
+// Connect to MongoDB
+const mongoUrl = config.mongo_connection;
+
+// Mangodb connection
+mongoose.set('strictQuery', false);
+mongoose.Promise = bluebird;
+
+mongoose.connect(mongoUrl)
+.then(
+    () => { console.log("DB Connection Successfull!") },
+).catch(err => {
+    console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+    // process.exit();
+});
+
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/api/v1', authRouter); // Auth route [ Login, Register & Logout]
+// Secure the app
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: config.key,
+    store: MongoStore.create({
+        mongoUrl: mongoUrl
+      }),
+    cookie: {
+    maxAge: 60 * 1000 * 5 // 5 min in milliseconds
+    }
+
+}));
+
+// Secure the app
+
+app.use(CSRF());
+app.use(helmet());
+
+app.use('/api/v1', authRouter); // Auth routes [ Login, Register & Logout]
+app.use('/', userRouter); // User routes 
 
 export default app;
