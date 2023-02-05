a5ae1ef35a79b0a827ef39d535c6a43722cabfcf
embarekmessai <m_mebarek@hotmail.com>
2023-02-05 17:44:34 +0100

    Refactor: Move db connexion to app js

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,4 @@
-import mongoose from 'mongoose';
 import app from './app'
-import * as dotenv from 'dotenv'
-
-dotenv.config()
 
 /**
  * Get port from environment and store in Express.
@@ -17,13 +13,13 @@ app.listen(process.env.PORT || 5000, () => {
 /**
  *  Mangodb connectio
  */
-mongoose.set('strictQuery', false);
+// mongoose.set('strictQuery', false);
 
-main().then(() => console.log("DB Connection Successfull!"))
-      .catch((err: any) => console.log(err));
+// main().then(() => console.log("DB Connection Successfull!"))
+//       .catch((err: any) => console.log(err));
 
-async function main(){
-    await mongoose.connect(process.env.MONGO_URL)
+// async function main(){
+//     await mongoose.connect(process.env.MONGO_URL)
             
-}
+// }
 
