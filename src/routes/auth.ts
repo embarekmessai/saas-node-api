ce95c198b7b396f6050b164c4511ff6d6735ffcd
embarekmessai <m_mebarek@hotmail.com>
2023-02-07 00:38:57 +0100

    Add: Create forgot password route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,19 @@
 import * as authController from "../app/controllers/authController";
 import express from "express";
-import { loginValidator,  registerValidator} from "../app/validations/userValidation";
+import { emailValidator, loginValidator,  registerValidator} from "../app/validations/userValidation";
 var router = express.Router();
 
-// Login route
+// Login routes
 router.post('/login', loginValidator, authController.login);
+router.get('/login', authController.getAuth);
 
-// Register route
+// Register routes
 router.post('/register', registerValidator ,authController.register);
+router.get('/register', authController.getAuth);
+
+// Forgot password routes
+router.post('/forgot-password', emailValidator, authController.passwordRestLink);
+router.get('/forgot-password', authController.getPasswordRestLink);
 
 // Logout route
 router.get('/logout', );
