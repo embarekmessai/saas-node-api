cf79f6e0ac4e3672e191ebb12d23d25be8d00205
embarekmessai <m_mebarek@hotmail.com>
2023-02-06 20:26:33 +0100

    Refactor: Add csrf cookie to tests regiter & login

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -12,14 +12,19 @@ describe('Get Login', ()=> {
 
 describe('Login Endpoint', () => {
   let user: UserDocument;
+  let csrfCookie: string;
   beforeEach(async () => {
-
+    // Create a user
     user = await User.create({
         firstname: 'test',
         lastname : 'test',
         email: 'test@example.com',
         password: 'test123',
     });
+
+    // Generate csrf token
+    const response = request(app).get('/api/v1/csrf-cookie');
+    csrfCookie = (await response).header['set-cookie'];
   });
 
   afterEach(async () => {
@@ -27,29 +32,25 @@ describe('Login Endpoint', () => {
   });
 
   it('should return 200 and access token', async () => {
-    await request(app).get('/api/v1/login', async(result) => {
-      await request(app)
-        .post('/api/v1/login')
-        .set('Cookie',['XSRF-TOKEN', '123456789'])
-        .send({
-          email: 'test@example.com',
-          password: 'test123',
-          _csrf: result._csrf
-        });
-      expect(200);
-      expect((res: request.Response) => (res.body).toHaveProperty('accessToken'));
-
-    })
+    await request(app)
+      .post('/api/v1/login')
+      .set('Cookie', csrfCookie)
+      .send({
+        email: 'test@example.com',
+        password: 'test123'
+      });
+    expect(200);
+    expect((res: request.Response) => (res.body).toHaveProperty('accessToken'));
     // expect(res.body).toHaveProperty('email', 'test@example.com');
   });
 
   it('should return 403 with invalid password', async () => {
     await request(app)
       .post('/api/v1/login')
+      .set('Cookie', csrfCookie)
       .send({
         email: 'test@example.com',
-        password: 'invalid',
-        _csrf: expect.any(String)
+        password: 'invalid'
       });
     expect(400);
     expect((res: request.Response) => (res.body).toHaveProperty('errors[0].msg', /The password is incorrect./));
@@ -57,18 +58,16 @@ describe('Login Endpoint', () => {
   });
 
   // Test user confirmation password not match
-  it('Login with not exiting email', ()=> {
+  it('Login with not exiting email', async() => {
         
-    request(app).get('/api/v1/csrf-cookie', async()=> {
-        await request(app)
-                .post('/api/v1/register')
-                .send({
-                    email: "test@test.com",
-                    password:"123456789"
-                })
-                .expect(400);
-                expect((res: request.Response) => (res.body).toHaveProperty('errors[0].msg', /The selected email is invalid./));
-                expect((res: request.Response) => (res.body).toHaveProperty('errors[0].param', 'email'));
-    });
+    await request(app)
+            .post('/api/v1/register')
+            .set('Cookie', csrfCookie)
+            .send({
+                email: "test@test.com",
+                password:"123456789"
+            })
+            .expect(400);
+            expect((res: request.Response) => (res.body).toHaveProperty('errors[0].msg', /The selected email is invalid./));
 });
 });
--- tests/register.test.ts
+++ tests/register.test.ts
@@ -4,42 +4,91 @@ import app from '../src/app';
 
 
 describe('Register Endpoint', ()=> {
+    let csrfCookie: string;
+
+    beforeEach(async()=> { 
+        const response = request(app).get('/api/v1/csrf-cookie');
+        csrfCookie = (await response).header['set-cookie'];
+    })
     afterEach(async () => {
         await User.deleteOne({email: 'test@test.com'});
     });
 
     // Test user registration
-    it('Regiter new user', ()=> {
-        request(app).get('/api/v1/csrf-cookie', async()=> {
-            await request(app)
-                    .post('/api/v1/register')
-                    .send({
-                        firstname: "test",
-                        lastname : "test",
-                        email: "test@test.com",
-                        password:"123456789",
-                        password_confirmation:"123456789"
-                    })
-                    .expect(200);
-        });
+    it('Regiter new user', async()=> {
+
+        await request(app)
+                .post('/api/v1/register')
+                .set('Cookie', csrfCookie)
+                .send({
+                    firstname: "test",
+                    lastname : "test",
+                    email: "test@test.com",
+                    password:"123456789",
+                    password_confirmation:"123456789"
+                })
+                .expect(200);
+        
     });
 
     // Test user confirmation password not match
-    it('Regiter new user with wrong confirmation password', ()=> {
-        
-        request(app).get('/api/v1/csrf-cookie', async()=> {
-            await request(app)
-                    .post('/api/v1/register')
-                    .send({
-                        firstname: "test",
-                        lastname : "test",
-                        email: "test@test.com",
-                        password:"123456789",
-                        password_confirmation:"123456789"
-                    })
-                    .expect(200);
-                    expect((res: request.Response) => (res.body).toHaveProperty('errors[0].msg', /Password confirmation do not match/));
-                    expect((res: request.Response) => (res.body).toHaveProperty('errors[0].param', 'password_confirmation'));
-        });
+    it('Regiter new user with wrong confirmation password', async() => {
+      
+        const response  = await request(app)
+                .post('/api/v1/register')
+                .set('Cookie', csrfCookie)
+                .send({
+                    firstname: "test",
+                    lastname : "test",
+                    email: "test@test.com",
+                    password:"123456789",
+                    password_confirmation:"123456"
+                })
+                .expect(400);
+                expect(response.body).toHaveProperty('errors[0].msg', "Password confirmation do not match");
+                expect(response.body).toHaveProperty('errors[0].param', 'password_confirmation');
     });
+
+});
+
+describe('Register with same email', ()=> {
+    let user: UserDocument;
+    let csrfCookie: string;
+    beforeEach( async() => {
+        user = await User.create({
+            firstname: 'test_01',
+            lastname : 'test_01',
+            email: 'test_01@test.com',
+            password: '123456789'
+        });
+        const response = request(app).get('/api/v1/csrf-cookie');
+        csrfCookie = (await response).header['set-cookie'];
+
+    });
+    
+    afterEach(async () => {
+        await User.deleteOne({email: 'test_01@test.com'});
+    });
+
+    // Test regiter user with existing email address
+    it('Regiter new user with existing email address', async()=> {
+
+        const response = await request(app)
+                .post('/api/v1/register')
+                .set('Cookie', csrfCookie)
+                .send({
+                    firstname: "test",
+                    lastname : "test",
+                    email: "test_01@test.com",
+                    password:"123456789",
+                    password_confirmation:"123456789"
+                })
+                .expect(500);
+                expect(response.body).toHaveProperty('errors[0].msg', "The email has already been taken.");
+                expect(response.body).toHaveProperty('errors[0].param', 'email');
+                
+    });
+
+        
+
 })
