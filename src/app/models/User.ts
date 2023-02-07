42a1989dc6f68b13beb61ba3f4cebf9aedcb499b
embarekmessai <m_mebarek@hotmail.com>
2023-02-07 12:43:25 +0100

    Refactor: Create a profile type Add: Linkdind field

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,5 +1,32 @@
 import { Schema, model, Document } from 'mongoose';
 
+export type Profile = {
+    isActive: Boolean,
+    mobile: string,
+    mobile_confirmation: string,
+    mobile_confirmed_at: string,
+    description: string,
+    address: string,
+    city: string,
+    postal_code: string,
+    country: string,
+    latitude: string,
+    longitude: string,
+    avatar: string,
+    last_login: string,
+    last_logout: string,
+    isOnline: string,
+    photos: string,
+    presentation: string,
+    whatsapp: string,
+    facebook: string,
+    youtube: string,
+    instagram: string,
+    snapshat: string,
+    tiktok: string,
+    linkedin: string
+}
+
 export type UserDocument = Document & {
     firstname: string;
     lastname: string;
@@ -10,31 +37,7 @@ export type UserDocument = Document & {
 
     accessToken: string;
 
-    profile: {
-        isActive: Boolean,
-        mobile: string,
-        mobile_confirmation: string,
-        mobile_confirmed_at: string,
-        description: string,
-        address: string,
-        city: string,
-        postal_code: string,
-        country: string,
-        latitude: string,
-        longitude: string,
-        avatar: string,
-        last_login: string,
-        last_logout: string,
-        isOnline: string,
-        photos: string,
-        presentation: string,
-        whatsapp: string,
-        facebook: string,
-        youtube: string,
-        instagram: string,
-        snapshat: string,
-        tiktok: string
-    };
+    profile: Profile;
     createdAt: Date;
     updatedAt: Date;
     comparePassword: comparePasswordFunction;
@@ -76,7 +79,8 @@ const userSchema = new Schema<UserDocument>(
             youtube: {type: String},
             instagram: {type: String},
             snapshat: {type: String},
-            tiktok:{type: String}
+            tiktok:{type: String},
+            linkedin:{type: String}
         }
     }, { timestamps: true }
     );
